2d088e00c9bfdc47191e0ababe4632260bcf42c7
Johny Ho <johny@thoughtbot.com>
2020-05-06 18:33:04 -0400

    Render Seats for a Venue with SVG elements
    For comparison [see this commit][commit]
    
    There's nothing spectacular to show off in this commit, but perhaps thats the
    point.
    
    Breezy is really a multi-page application. We include all the content
    together on the same `index.json.props` just as we would when we're
    working with just a single `index.html.erb`. Then we modify the JSX markup to
    account for the new incoming props.
    
    Of note! The very next commit is a cherry-pick of the test on the original
    select-your-own-seat branch. There were no additional changes to make the
    tests work!
    
    [commit]: https://github.com/seanpdoyle/select-your-own-seat/commit/216b59c57382d8d091ee380dac22779bce0f03f3

--- app/views/seats/index.jsx
+++ app/views/seats/index.jsx
@@ -4,13 +4,33 @@ import {connect} from 'react-redux'
 import Layout from 'components/Layout'
 import * as applicationActionCreators from 'javascript/packs/action_creators'
 
+const buildSectionElements = (sections) => {
+  return sections.map((section) => {
+    const seatElements = section.seats.map(({x, y}) => (
+      <svg width="12px" height="12px" viewBox="0 0 24 24" x={x} y={y}>
+        <circle fill="#37b24d" r="12" cx="12" cy="12"></circle>
+        <circle fill="#ffffff" r="6" cx="12" cy="12"></circle>
+      </svg>
+    ))
+
+    return <g>{seatElements}</g>
+  })
+}
+
 class SeatsIndex extends React.Component {
   render () {
+    const {
+      venueName,
+      sections,
+    } = this.props
+
+    const sectionElements = buildSectionElements(sections)
+
     return (
       <Layout {...this.props}>
         <header className="syos-site-frame__header syos-site-header">
           <p className="syos-site-header__subtext">
-            Venue Name
+            {venueName}
           </p>
         </header>
 
@@ -28,7 +48,8 @@ class SeatsIndex extends React.Component {
                 viewBox="0 0 1600 1600"
               >
                 <rect fill="none" x="0" y="0" width="1600" height="1600"></rect>
-              </svg>
+                { sectionElements }
+                </svg>
             </div>
 
             <div className="syos-frame__sidebar">
