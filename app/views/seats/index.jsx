631e21b07a02195820f5943fc6ca130c3abffe4e
Johny Ho <johny@thoughtbot.com>
2020-05-08 10:40:08 -0400

    Refactor: Extract SeatingMap component [NEW]
    This commit has no equivalent in the Stimulus branch. Here, a concept of a
    `SeatingMap` component emerges from `index.jsx`. This is a quality of working in
    React. The library library makes it easy to extract related and sematic behavior
    into into reusable, testable components.
    
    Rails has tools like partial, presenters, view object, etc. But when faced with
    a ERB template with a rich history of changes, dependencies on instance variables,
    global view helpers, local variables contribute to some of the difficulty of
    extracting isolated, and testable behavior.
    
    This might change though as I hear a Cell like library is coming from Github.

--- app/views/seats/index.jsx
+++ app/views/seats/index.jsx
@@ -4,29 +4,9 @@ import {connect} from 'react-redux'
 import BaseScreen from 'components/BaseScreen'
 import SeatDialog from 'components/SeatDialog'
 import Cart from 'components/Cart'
+import SeatingMap from 'components/SeatingMap'
 import Layout from 'components/Layout'
 import * as applicationActionCreators from 'javascript/packs/action_creators'
-
-const buildSectionElements = (sections) => {
-  return sections.map((section) => {
-    const seatElements = section.seats.map((seat) => (
-      <a
-        href={seat.venueFloorSeatPath}
-        aria-label={seat.ariaLabel}
-       >
-        <use
-          width="12px"
-          height="12px"
-          xlinkHref={seat.href}
-          x={seat.x}
-          y={seat.y}
-        />
-      </a>
-    ))
-
-    return <g>{seatElements}</g>
-  })
-}
 
 class SeatsIndex extends BaseScreen {
   render () {
@@ -36,8 +16,6 @@ class SeatsIndex extends BaseScreen {
       seat,
       cart
     } = this.props
-
-    const sectionElements = buildSectionElements(sections)
 
     return (
       <Layout {...this.props}>
@@ -53,39 +31,8 @@ class SeatsIndex extends BaseScreen {
             className="syos-frame"
           >
             <div className="syos-frame__map">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                xmlnsXlink="http://www.w3.org/1999/xlink"
-                className="syos-seat-map"
-                width="1600px"
-                height="1600px"
-                viewBox="0 0 1600 1600"
-              >
-                <rect fill="none" x="0" y="0" width="1600" height="1600"></rect>
-                <svg style={{display: "none"}}>
-                  <symbol
-                    id="seat-icon-unselected"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                  >
-                    <circle fill="#37b24d" r="12" cx="12" cy="12"></circle>
-                    <circle fill="#ffffff" r="6" cx="12" cy="12"></circle>
-                  </symbol>
-
-                  <symbol
-                    id="seat-icon-selected"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                  >
-                    <circle fill="#37b24d" r="12" cx="12" cy="12"></circle>
-                  </symbol>
-                </svg>
-                { sectionElements }
-              </svg>
+              <SeatingMap sections={sections} />
             </div>
-
             <div className="syos-frame__sidebar">
               <Cart cart={cart} />
             </div>
