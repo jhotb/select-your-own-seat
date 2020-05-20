a0adeba74bd263518cf2eaa1c487299981e4a7d6
Johny Ho <johny@thoughtbot.com>
2020-05-19 22:16:56 -0400

    Unselect a Seat to remove it from visitor's Cart
    For comparison [see this commit][commit]
    
    Nothing exciting here, again the egronomics is similar to the Stimulus version.
    
    [commit]: https://github.com/seanpdoyle/select-your-own-seat/commit/3cd6b13369dcc83c73139e0f30c5b541da35678d

--- app/components/Cart.jsx
+++ app/components/Cart.jsx
@@ -3,14 +3,12 @@ import RailsTag from '@jho406/breezy/dist/RailsTag'
 
 export default class extends React.Component {
   render () {
-    const cartItems = this.props.cart.map(({rowNumber, price, removeSvg, id}) => (
+    const cartItems = this.props.cart.map(({rowNumber, price, removeButton, id}) => (
       <tr key={id}>
         <td> {rowNumber} </td>
         <td className="syos-table__cell--numerals"> {price} </td>
         <td className="syos-u-text-align-right">
-          <button className="syos-button syos-button--transparent">
-            <RailsTag html={ removeSvg } />
-          </button>
+          <RailsTag html={ removeButton } />
         </td>
       </tr>
     ))
